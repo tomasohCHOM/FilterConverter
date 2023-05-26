4fd83e2e5b484af6fc170a0204ae3324cf37f04a
Tomas Oh <tomasoh@csu.fullerton.edu>
2023-05-26 12:46:17 -0700

    Finalized

--- scripts/index.ts
+++ scripts/index.ts
@@ -16,12 +16,38 @@ function hexToRgb(hex: string): number[] {
      ] : null;
 }
 
+function RgbToArray(rgb: string): number[] {
+    // const rgbArray = rgb.replace(/[^\d,]/g, "").split(",");
+    // return rgbArray ? [
+    //     parseInt(rgbArray[1]),
+    //     parseInt(rgbArray[2]),
+    //     parseInt(rgbArray[3])
+    // ] : null;
+    return rgb.match(/\d+/g).map(Number);
+}
+
 const performConvertion = document.querySelector(".perform-convertion");
 const target: HTMLInputElement = document.querySelector(".target");
+const rgbOrHexSelection: HTMLSelectElement = document.getElementById("select-color-type") as HTMLSelectElement;
+const placeholders = {
+    rgb: "e.g 'rgb(121, 201, 176)'",
+    hex: "e.g '00a4d3' or '#00a4d3'"
+}
 
+let isHexSelected = false;
+
+rgbOrHexSelection.addEventListener("change", ()=> {
+    let selectedValue = rgbOrHexSelection.options[rgbOrHexSelection.selectedIndex].value;
+    console.log(selectedValue);
+    isHexSelected = selectedValue === "hex";
+    target.placeholder = isHexSelected ? (placeholders.hex) : (placeholders.rgb);
+});
+
+// Event Listener for performing the Convertion
 performConvertion.addEventListener("click", () => {
-    const rgb: number[] = hexToRgb(target.value);
-    if (rgb.length !== 3) {
+    const rgb: number[] = isHexSelected ? hexToRgb(target.value) : RgbToArray(target.value);
+
+    if (rgb == null || rgb.length !== 3) {
         alert("Invalid format!");
         return;
     }
@@ -50,4 +76,17 @@ performConvertion.addEventListener("click", () => {
     const filterDetail: HTMLElement = document.querySelector(".filter-detail");
     filterDetail.innerText = result.filter;
     document.querySelector(".loss-detail").innerHTML = `Loss: ${result.loss.toFixed(1)}. <b>${lossMessage}</b>`;
+});
+
+// Toggle Sidebar On/Off
+const openBtn = document.getElementById("open-more-info");
+const closeBtn = document.getElementById("close-more-info");
+const moreInfoBar = document.querySelector(".more-info");
+
+openBtn.addEventListener("click", () => {
+    moreInfoBar.classList.add("show");
+});
+
+closeBtn.addEventListener("click", () => {
+    moreInfoBar.classList.remove("show");
 });
